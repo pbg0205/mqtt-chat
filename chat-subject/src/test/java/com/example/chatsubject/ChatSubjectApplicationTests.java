910fe960c2b9d23ebd25124e30d17941eacb4d0a
cooper <pbg0205@naver.com>
2021-12-13 16:40:27 +0900

    project first commit

---
+++ chat-subject/src/test/java/com/example/chatsubject/ChatSubjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatsubject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatSubjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
