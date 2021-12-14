2e11dafbdbcf9e3b529326a35f545873870abef3
cooper <pbg0205@naver.com>
2021-12-14 11:17:47 +0900

    feat:  Member entity 추가
    - 빈 아이디 작성 시, IllegalArgumentException 반환
    
    - 빈 패스워드 작성 시, IllegalArgumentException 반환

---
+++ chat-subject/src/main/java/com/example/chatsubject/account/domain/Member.java
@@ -0,0 +1,50 @@
+package com.example.chatsubject.account.domain;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Table(name = "member")
+@EqualsAndHashCode(of = {"id", "password"})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id",nullable = false)
+    private Long id;
+
+    @Column(name = "member_name",nullable = false)
+    private String name;
+
+    @Column(name = "member_password", nullable = false)
+    private String password;
+
+    public Member(String name, String password) {
+        this.name = name;
+        this.password = password;
+        validate();
+    }
+
+    private void validate() {
+        validateName();
+        validatePassword();
+    }
+
+    private void validateName() {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("이름이 존재하지 않습니다.");
+        }
+    }
+
+    private void validatePassword() {
+        if (password == null || password.isEmpty()) {
+            throw new IllegalArgumentException("비밀번호가 존재하지 않습니다.");
+        }
+    }
+}
