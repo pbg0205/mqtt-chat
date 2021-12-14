a4d3e67397e19bb0fb033d8686cac48115de8901
cooper <pbg0205@naver.com>
2021-12-14 14:32:36 +0900

    feat: Member 필드에 이메일 추가

--- chat-subject/src/main/java/com/example/chatsubject/account/domain/Member.java
+++ chat-subject/src/main/java/com/example/chatsubject/account/domain/Member.java
@@ -3,6 +3,8 @@ package com.example.chatsubject.account.domain;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Entity
 @Getter
@@ -16,22 +18,27 @@ public class Member {
     @Column(name = "member_id",nullable = false)
     private Long id;
 
-    @Column(name = "member_name",nullable = false)
+    @Column(name = "member_name",nullable = false, unique = true)
     private String name;
+
+    @Column(name = "member_email", nullable = false)
+    private String email;
 
     @Column(name = "member_password", nullable = false)
     private String password;
 
     @Builder
-    public Member(String name, String password) {
+    public Member(String name, String password, String email) {
         this.name = name;
         this.password = password;
+        this.email = email;
         validate();
     }
 
     private void validate() {
         validateName();
         validatePassword();
+        validateEmail();
     }
 
     private void validateName() {
@@ -43,6 +50,15 @@ public class Member {
     private void validatePassword() {
         if (password == null || password.isEmpty()) {
             throw new IllegalArgumentException("비밀번호가 존재하지 않습니다.");
+        }
+    }
+
+    private void validateEmail() {
+        Pattern pattern = Pattern.compile("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$");
+        Matcher matcher = pattern.matcher(email);
+
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("이메일이 올바르지 않습니다.");
         }
     }
 }
