448d50cb7b9a038c4a6c789ffba1c8db1a8efe30
cooper <pbg0205@naver.com>
2021-12-18 17:16:35 +0900

    feat: 중복 로그인 예외처리 추가

--- chat-subject/src/main/java/com/example/chatsubject/account/domain/MemberRepository.java
+++ chat-subject/src/main/java/com/example/chatsubject/account/domain/MemberRepository.java
@@ -3,4 +3,5 @@ package com.example.chatsubject.account.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    boolean existsByEmail(String email);
 }
--- chat-subject/src/main/java/com/example/chatsubject/account/service/MemberService.java
+++ chat-subject/src/main/java/com/example/chatsubject/account/service/MemberService.java
@@ -5,6 +5,7 @@ import com.example.chatsubject.account.domain.Member;
 import com.example.chatsubject.account.domain.MemberRepository;
 import com.example.chatsubject.account.dto.MemberSignUpRequest;
 import com.example.chatsubject.account.dto.MemberSignUpResponse;
+import com.example.chatsubject.account.exception.DuplicatedUserException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,12 +17,19 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     @Transactional
-    public MemberSignUpResponse signUpMember(MemberSignUpRequest memberSignUpResponse) {
-        Member member = memberSignUpResponse.toEntity();
+    public MemberSignUpResponse signUpMember(MemberSignUpRequest memberSignUpRequest) {
+        Member member = memberSignUpRequest.toEntity();
+        validateDuplicatedUser(memberSignUpRequest);
         Member savedMember = memberRepository.save(member);
         return MemberSignUpResponse.builder()
                 .name(savedMember.getNickname())
                 .email(savedMember.getEmail())
                 .build();
+    }
+
+    private void validateDuplicatedUser(MemberSignUpRequest memberSignUpRequest) throws DuplicatedUserException {
+        if (memberRepository.existsByEmail(memberSignUpRequest.getEmail())) {
+            throw new DuplicatedUserException("기존 회원이 존재합니다.");
+        }
     }
 }
