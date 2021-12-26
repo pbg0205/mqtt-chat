13975b9309b4ea33d4cdadc08b2492db18592b4e
cooper <pbg0205@naver.com>
2021-12-27 08:23:08 +0900

    feat: 채팅 내용 저장 로직 추가
    - embbeded redis를 이용한 저장 로직

---
+++ chat-subject/src/main/java/com/example/chatsubject/chat/domain/ChatContentRepository.java
@@ -0,0 +1,6 @@
+package com.example.chatsubject.chat.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ChatContentRepository extends CrudRepository <ChatContent, Long> {
+}
