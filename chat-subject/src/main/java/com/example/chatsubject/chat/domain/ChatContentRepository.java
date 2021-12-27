965a27a05e2a48afad5406a0fbb62cdf739ce9b4
cooper <pbg0205@naver.com>
2021-12-27 17:05:28 +0900

    feat: mqtt chatting message 추가 로직
    - integration flow 요청 로직 추가 필요

--- chat-subject/src/main/java/com/example/chatsubject/chat/domain/ChatContentRepository.java
+++ chat-subject/src/main/java/com/example/chatsubject/chat/domain/ChatContentRepository.java
@@ -1,6 +1,8 @@
 package com.example.chatsubject.chat.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ChatContentRepository extends CrudRepository <ChatContent, Long> {
 }
