df1d7a1bef1bdd08630f53afa5490ac51d1f942d
cooper <pbg0205@naver.com>
2022-01-03 14:57:26 +0900

    test: 불필요 테스트 제거

--- chat-subject/src/test/java/com/example/chatsubject/account/domain/UserTest.java
+++ chat-subject/src/test/java/com/example/chatsubject/account/domain/UserTest.java
@@ -22,45 +22,4 @@ class UserTest {
         assertThat(user.getEmail()).isEqualTo(new Email("cooper@rsupport.com"));
     }
 
-    @Test
-    @DisplayName("이름을 기재하지 않을 경우, IllegalArgumentException을 던진다.")
-    void retrieve_illegal_argument_exception_when_name_is_empty() {
-        assertThrows(
-                IllegalArgumentException.class,
-                () -> User.builder()
-                        .nickname("")
-                        .password("123")
-                        .email("cooper@rsupport.com")
-                        .build(),
-                "이름이 존재하지 않습니다."
-        );
-    }
-
-    @Test
-    @DisplayName("비밀번호를 기재하지 않을 경우, IllegalArgumentException을 던진다.")
-    void retrieve_illegal_argument_exception_when_password_is_empty() {
-        assertThrows(
-                IllegalArgumentException.class,
-                () -> User.builder()
-                        .email("123@rsupport.com")
-                        .password("")
-                        .nickname("cooper")
-                        .build(),
-                "비밀번호가 존재하지 않습니다."
-        );
-    }
-
-    @Test
-    @DisplayName("올바른 이메일 형태가 아닐 경우, IllegalArgumentException을 반환한다.")
-    void retrieve_illegal_argument_exception_when_input_is_not_email_format() {
-        assertThrows(
-                IllegalArgumentException.class,
-                () -> User.builder()
-                        .email("coo@.com")
-                        .password("123")
-                        .nickname("cooper")
-                        .build(),
-                "이메일이 올바르지 않습니다."
-        );
-    }
 }
