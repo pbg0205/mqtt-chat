59540ed5d63bef269b371b2e181e1fb29dbf4dfe
cooper <pbg0205@naver.com>
2022-01-04 23:07:49 +0900

    feat: 인증 시, session 객체 DTO 추가 방법 변경
    - 기존 AuthProvider를 통한 UsernamePasswordAuthenticationToken 추가 로직 변경
    
    - UserService에 UserDetailsService 선언하여 loadUserByUsername 메서드의 반환값으로 변경

--- chat-subject/src/main/java/com/example/chatsubject/account/domain/User.java
+++ chat-subject/src/main/java/com/example/chatsubject/account/domain/User.java
@@ -5,6 +5,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.Column;
@@ -14,13 +17,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Collection;
+import java.util.Collections;
 
 @Entity
 @Getter
 @ToString
 @Table(name = "users")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -50,4 +55,33 @@ public class User {
         this.password = passwordEncoder.encode(password);
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(authority));
+    }
+
+    @Override
+    public String getUsername() {
+        return email.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- chat-subject/src/main/java/com/example/chatsubject/common/config/WebSecurityConfig.java
+++ chat-subject/src/main/java/com/example/chatsubject/common/config/WebSecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 
 @Configuration
