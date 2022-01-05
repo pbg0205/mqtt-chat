55d43472fb1865401183b15bd9a7ab1ee1bb9fae
cooper <pbg0205@naver.com>
2022-01-06 08:29:45 +0900

    feat: 로그아웃 기능 활성화

--- chat-subject/src/main/java/com/example/chatsubject/common/config/WebSecurityConfig.java
+++ chat-subject/src/main/java/com/example/chatsubject/common/config/WebSecurityConfig.java
@@ -31,6 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.logout()
                 .logoutSuccessUrl("/")
+                .deleteCookies("JSESSIONID")
                 .permitAll();
     }
 
