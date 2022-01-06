35700ef6ff00decf9231708b6e9862144d5ba543
cooper <pbg0205@naver.com>
2022-01-06 16:25:28 +0900

    feat: chatmessage ToString 추가
    - 정상 출력 메세지 확인을 위한 @ToString 추가

--- chat-subject/src/main/java/com/example/chatsubject/chat/domain/ChatMessage.java
+++ chat-subject/src/main/java/com/example/chatsubject/chat/domain/ChatMessage.java
@@ -1,13 +1,16 @@
 package com.example.chatsubject.chat.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 
-@Setter
 @Getter
+@Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChatMessage {
