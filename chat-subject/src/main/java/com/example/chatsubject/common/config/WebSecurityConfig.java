d1c9667c8c7ca552f79a6350af5180018faf9e28
cooper <pbg0205@naver.com>
2022-01-10 20:52:00 +0900

    refactor: H2 설정 및 관련 security config 변경

--- chat-subject/src/main/java/com/example/chatsubject/common/config/WebSecurityConfig.java
+++ chat-subject/src/main/java/com/example/chatsubject/common/config/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package com.example.chatsubject.common.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,7 +9,6 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 
-@Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -19,7 +17,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .mvcMatchers("/", "/signup").permitAll()
-                .mvcMatchers("/chat/*").authenticated();
+                .mvcMatchers("/chat/**").authenticated();
+
+        http.csrf().ignoringAntMatchers("/h2-console/**");
+        http.headers().frameOptions().disable();
 
         http.formLogin()
                 .loginPage("/login")
@@ -28,7 +29,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.logout()
                 .logoutSuccessUrl("/")
-                .deleteCookies("JSESSIONID")
                 .permitAll();
 
         http.sessionManagement()
