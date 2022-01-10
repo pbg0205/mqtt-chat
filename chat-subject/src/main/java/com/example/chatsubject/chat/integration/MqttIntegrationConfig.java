888b19b7fb851245e199efb4f52d180a998291fe
cooper <pbg0205@naver.com>
2022-01-10 11:31:01 +0900

    refactor: mqtt 전송 확인용 mqttPayloadHandler 제거

--- chat-subject/src/main/java/com/example/chatsubject/chat/integration/MqttIntegrationConfig.java
+++ chat-subject/src/main/java/com/example/chatsubject/chat/integration/MqttIntegrationConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.Transformers;
-import org.springframework.integration.handler.GenericHandler;
 import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
 import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
 import org.springframework.messaging.MessageHandler;
@@ -22,7 +21,6 @@ public class MqttIntegrationConfig {
     @Bean
     public IntegrationFlow chatMessageSaveFlow() {
         return IntegrationFlows.from(inboundChannel())
-                .handle(mqttPayloadHandler())
                 .transform(Transformers.fromJson(ChatMessage.class))
                 .handle(chatMessageSaveHandler())
                 .get();
@@ -39,15 +37,6 @@ public class MqttIntegrationConfig {
         channelAdapter.setConverter(new DefaultPahoMessageConverter());
         channelAdapter.setQos(1);
         return channelAdapter;
-    }
-
-    @Bean
-    public GenericHandler<Object> mqttPayloadHandler() {
-        return ((payload, headers) -> {
-            System.out.println("headers: " + headers);
-            System.out.println("payload: " + payload);
-            return payload;
-        });
     }
 
     @Bean
