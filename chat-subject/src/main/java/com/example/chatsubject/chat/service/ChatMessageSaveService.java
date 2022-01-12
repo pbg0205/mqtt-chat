edfe3c175d6d9267d47be3a2410ae1f7d6f1d5bd
cooper <pbg0205@naver.com>
2022-01-12 14:01:03 +0900

    feat: 채팅 메세지 저장 로직 구현
    - 채팅방의 존재 여부 확인 후, 채팅방에 메세지를 추가하는 방식으로 구현

---
+++ chat-subject/src/main/java/com/example/chatsubject/chat/service/ChatMessageSaveService.java
@@ -0,0 +1,34 @@
+package com.example.chatsubject.chat.service;
+
+import com.example.chatsubject.chat.domain.ChatMessage;
+import com.example.chatsubject.chat.domain.ChatMessageRepository;
+import com.example.chatsubject.chat.domain.ChatRoom;
+import com.example.chatsubject.chat.domain.ChatRoomRepository;
+import com.example.chatsubject.chat.dto.ChatMessageSaveRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ChatMessageSaveService {
+
+    private final ChatRoomRepository chatRoomRepository;
+    private final ChatMessageRepository chatMessageRepository;
+
+    public void save (ChatMessageSaveRequest saveRequest){
+        Optional<ChatRoom> chatRoomOptional = chatRoomRepository.findById(saveRequest.getChatRoomId());
+
+        if (chatRoomOptional.isPresent()) {
+            ChatRoom chatRoom = chatRoomOptional.get();
+            ChatMessage chatMessage = saveRequest.toEntity();
+            chatMessageRepository.save(chatMessage);
+            chatRoom.addChatMessage(chatMessage);
+        }
+    }
+
+}
