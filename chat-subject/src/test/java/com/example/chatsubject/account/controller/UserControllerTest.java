98940b4be88c0e5c6d589c1cd47e353e6ed01d37
cooper <pbg0205@naver.com>
2022-01-19 20:15:08 +0900

    test: 회원가입 내용 미입력시, 회원가입 페이지 렌더링 테스트 코드

--- chat-subject/src/test/java/com/example/chatsubject/account/controller/UserControllerTest.java
+++ chat-subject/src/test/java/com/example/chatsubject/account/controller/UserControllerTest.java
@@ -3,13 +3,20 @@ package com.example.chatsubject.account.controller;
 import com.example.chatsubject.account.service.UserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.stream.Stream;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -40,6 +47,29 @@ class UserControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("signup"))
                 .andExpect(model().attributeExists("userSignUpRequest"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provide_sign_up_input_for_value_is_null_or_blank")
+    @DisplayName("회원가입 입력 값을 입력하지 않을 경우, 회원가입 페이지를 렌더링한다.")
+    void should_render_sign_up_page_when_input_value_is_blank_and_null(String nickname,
+                                                                       String password,
+                                                                       String email) throws Exception {
+        mockMvc.perform(post("/signup")
+                        .param("nickname", nickname)
+                        .param("password", password)
+                        .param("email", email)
+                        .with(csrf())
+                ).andDo(print())
+                .andExpect(view().name("signup"));
+    }
+
+    private static Stream<Arguments> provide_sign_up_input_for_value_is_null_or_blank() {
+        return Stream.of(
+                Arguments.of("", "password", "sample@sample.com"),
+                Arguments.of("nickname", "", "sample@sample.com"),
+                Arguments.of("nickname", "password", "")
+        );
     }
 
 }
