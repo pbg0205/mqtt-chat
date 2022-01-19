bbe66ffbaa887479025b94071e260efa7b100e0d
cooper <pbg0205@naver.com>
2022-01-19 09:44:05 +0900

    refactor: column name prefix 제거 및 불필요 annotaion 삭제

--- chat-subject/src/main/java/com/example/chatsubject/chat/domain/ChatMessage.java
+++ chat-subject/src/main/java/com/example/chatsubject/chat/domain/ChatMessage.java
@@ -1,13 +1,11 @@
 package com.example.chatsubject.chat.domain;
 
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -17,33 +15,25 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @ToString
-@Table(name = "chat_message")
-@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ChatMessage {
 
     @Id
-    @Column(name = "chat_message_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "chat_message_message")
     private String message;
 
-    @Column(name = "chat_message_writer_name")
     private String writerName;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "chat_message_message_type")
     private MessageType messageType;
 
-    @Column(name = "chat_message_created_at")
     private LocalDateTime createAt;
 
     @JoinColumn(name="chat_room_id")
