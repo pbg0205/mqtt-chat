43847e72a8ca78b88267eee39ad04e6483f067a1
cooper <pbg0205@naver.com>
2022-01-19 17:05:36 +0900

    test: 로그인 페이지 렌더링 테스트 코드

---
+++ chat-subject/src/test/java/com/example/chatsubject/account/controller/UserControllerTest.java
@@ -0,0 +1,34 @@
+package com.example.chatsubject.account.controller;
+
+import com.example.chatsubject.account.service.UserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+@WebMvcTest(controllers = UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+
+    @Test
+    @DisplayName("로그인 페이지를 렌더링한다.")
+    void should_render_login_page() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"));
+    }
+
+}
