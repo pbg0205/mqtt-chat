1177e4b43ce768b4a0358a0464867b9b8d052aa9
cooper <pbg0205@naver.com>
2022-01-19 09:45:08 +0900

    refactor: RequiredArgsConstructor 접근레벨 private 변경

--- chat-subject/src/main/java/com/example/chatsubject/chat/dto/ChatRoomLookupResponse.java
+++ chat-subject/src/main/java/com/example/chatsubject/chat/dto/ChatRoomLookupResponse.java
@@ -1,11 +1,12 @@
 package com.example.chatsubject.chat.dto;
 
 import com.example.chatsubject.chat.domain.ChatRoom;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@RequiredArgsConstructor (access = AccessLevel.PRIVATE)
 public class ChatRoomLookupResponse {
 
     private final Long roomId;
