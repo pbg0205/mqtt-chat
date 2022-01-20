2bfae5ce3e8eb24d35b54b0425bede912aabe6fd
cooper <pbg0205@naver.com>
2022-01-20 11:08:43 +0900

    test: 특정 채팅방 접속 여부 테스트 코드 작성

--- chat-subject/src/test/java/com/example/chatsubject/chat/controller/ChatRoomsControllerTest.java
+++ chat-subject/src/test/java/com/example/chatsubject/chat/controller/ChatRoomsControllerTest.java
@@ -1,6 +1,9 @@
 package com.example.chatsubject.chat.controller;
 
+import com.example.chatsubject.account.domain.User;
+import com.example.chatsubject.account.dto.UserDetailsResponse;
 import com.example.chatsubject.account.service.UserService;
+import com.example.chatsubject.chat.dto.ChatRoomDetailsResponse;
 import com.example.chatsubject.chat.service.ChatRoomService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,10 +13,15 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 
 @WebMvcTest
@@ -46,4 +54,33 @@ class ChatRoomsControllerTest {
                 .andExpect(redirectedUrlPattern("**/login"));
     }
 
+    @Test
+    @DisplayName("로그인한 사용자는 특정 채팅방에 입장할 수 있다.")
+    void can_join_chat_room_when_user_is_login() throws Exception {
+        UserDetailsResponse userDetailsResponse = UserDetailsResponse.from(User.builder()
+                .email("sample@sample.com")
+                .nickname("sample")
+                .password("password")
+                .build()
+        );
+
+        when(chatRoomService.getChatRoomDetails(1L)).thenReturn(
+                ChatRoomDetailsResponse.fromEntity(1L, "채팅방1", new ArrayList<>())
+        );
+
+        mockMvc.perform(get("/chat/rooms/1")
+                        .with(user(userDetailsResponse)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("chatroom-detail"));
+    }
+
+    @Test
+    @DisplayName("로그인하지 않는 사용자는 특정 채팅방에 입장할 수 없다.")
+    void can_not_join_room_when_user_is_not_authenticated() throws Exception {
+        mockMvc.perform(get("/chat/rooms/1"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrlPattern("**/login"));
+    }
 }
